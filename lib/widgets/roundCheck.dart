1b7d12cf4b14d0557549ef2a7c329838bff05f8c
HappyReni <dnjsvlfwo@gmail.com>
2022-11-22 00:22:51 +0900

    test

--- lib/widgets/roundCheck.dart
+++ lib/widgets/roundCheck.dart
@@ -26,7 +26,7 @@ class _RoundCheckState extends State<RoundCheck> {
   @override
   Widget build(BuildContext context) {
     return RoundCheckBox(
-      isChecked: widget.isModify,
+      isChecked: widget.isModify,f
       uncheckedWidget: Center(
         child: Text(
           '${widget.day.day}',
